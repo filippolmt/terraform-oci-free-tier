3b511ed2fbd0934514a433759f0d5daad9bd0488
filippo.merante <filippo.merante@dbminternational.it>
2023-10-03 20:25:57 +0200

    add backup volume

--- outputs.tf
+++ outputs.tf
@@ -12,13 +12,3 @@ output "public_ip" {
   description = "The public IP of the instance"
   value       = [oci_core_public_ip.public_ip.ip_address]
 }
-
-output "connect_with_ssh" {
-  description = "The command to connect to the instance with SSH"
-  value       = [oci_core_instance_console_connection.instance_console_connection.connection_string]
-}
-
-output "connect_with_vnc" {
-  description = "The command to connect to the instance with VNC"
-  value       = [oci_core_instance_console_connection.instance_console_connection.vnc_connection_string]
-}

46ecb4a118a1403952d1d1fc99b4a759695deb0e
Filippo <filippolmt@gmail.com>
2024-09-07 08:41:54 +0200

    fix

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -9,8 +9,8 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+  "deb [arch=\"\$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    \"\$\( . /etc/os-release && echo \"\$VERSION_CODENAME\" \)\" stable" |
   sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
@@ -56,7 +56,7 @@ while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do
 
   sudo mount -a
 
-  sleep 1
+  sleep 5
   ATTEMPT=$((ATTEMPT + 1))
 done
 
